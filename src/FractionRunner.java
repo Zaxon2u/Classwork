164b150414e84a3cfd4e5d1bcbb405de2b2fb83e
ceb-staff <zacklewis017@gmail.com>
2018-02-27 09:19:59 -0600

    fraction runner

---
+++ src/FractionRunner.java
@@ -0,0 +1,9 @@
+public class FractionRunner
+{
+    public static void main(String[] args)
+    {
+        Fraction f1 = new Fraction
+        Fraction f2 = new Fraction
+        System.out.println(f1.compareTo(f2));
+    }
+}
